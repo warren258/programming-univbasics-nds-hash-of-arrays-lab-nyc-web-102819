7457f11c25c68788b265483c976737236bab46a4
Warren McDonald <warrenjmac@mail.com>
2019-10-22 18:23:54 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,6 +9,6 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   BASE_HOA[:chipmunks]<< "Dave";
-  BASE_HOA[:third_earthers]<< "Snark";
+  BASE_HOA[:third_earthers]<< "Snarf";
   BASE_HOA[:jetsons]<< "Astro"
 end
